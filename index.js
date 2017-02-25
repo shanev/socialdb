bea126968a77ea5dc5e9932dde50733e9e7982c0
Shane Vitarana <shanev@gmail.com>
2017-02-25 18:56:01 -0500

    Get list of pending, requested, and accepted followers

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+/*
+  Social graph for user friends and relationships.
+  API:
+    Actions:
+    * follow(fromId, toId)
+    * unfollow(fromId, toId)
+
+    Data:
+    * pending(userId)
+    * requested(userId)
+    * accepted(userId)
+    * friends(userId) (alias of accepted)
+ */
 class SocialGraph {
   constructor(redis = null) {
     this.redis = redis;
   }
 
+  /**
+   * Creates a relationship between fromId and toId
+   * i.e:
+   *   user 1 wants to follow user 11:
+   *     follow(1, 11)
+   *     user:1:requested 11
+   *     user:11:pending 1
+   *   user 11 follows user 1 back:
+   *     follow(11, 1)
+   *     user:1:requested n/a
+   *     user:11:pending n/a
+   *     user:1:accepted 11
+   *     user:11:accepted 1
+   */
   follow(fromId, toId) {
     this.redis.multi()
       .sadd(`user:${fromId}:requested`, toId)
@@ -12,10 +39,18 @@ class SocialGraph {
     return new Promise((resolve) => { resolve(true); });
   }
 
-  // followers(userId, callback) {
-  //   // this.redis.smembers(`user:${userId}:followers`, (err, res) => (callback(res)));
-  //   return callback([1, 2, 3]);
-  // }
+  requested(userId, callback) {
+    this.redis.smembers(`user:${userId}:requested`, (err, res) => (callback(res)));
+  }
+
+  pending(userId, callback) {
+    this.redis.smembers(`user:${userId}:pending`, (err, res) => (callback(res)));
+  }
+
+  accepted(userId, callback) {
+    this.redis.smembers(`user:${userId}:accepted`, (err, res) => (callback(res)));
+  }
+
 }
 
 module.exports = SocialGraph;
--- test/test.js
+++ test/test.js
@@ -12,13 +12,13 @@ const SocialGraph = require('../index.js');
 
 const sg = new SocialGraph(client);
 
-describe('Relationships', () => {
+describe('requesting friends', () => {
   before(() => {
     client.flushdb();
   });
 
-  describe('#follow()', () => {
-    it('should allow you to follow', (done) => {
+  describe('.follow()', () => {
+    it('should add users to `requested` and `pending`', (done) => {
       sg.follow(1, 11).then((success) => {
         assert(success);
         client.smembers(`user:${1}:requested`, (err, res) => {
@@ -30,13 +30,47 @@ describe('Relationships', () => {
         });
       });
     });
-  });
 
-  // describe('#followers()', () => {
-  //   it('should get a list of followers', () => {
-  //     sg.followers(1, (users) => {
-  //       assert.equal(users.length, 5);
-  //     });
-  //   });
-  // });
+    describe('should query `pending`, `requested`, and `accepted`', () => {
+      describe('.requested()', () => {
+        it('should get a list of requested followers', (done) => {
+          sg.requested(1, (users) => {
+            assert.equal(users.length, 1);
+            done();
+          });
+        });
+      });
+
+      describe('.pending()', () => {
+        it('should get a list of pending followers', (done) => {
+          sg.pending(11, (users) => {
+            assert.equal(users.length, 1);
+            done();
+          });
+        });
+      });
+
+      describe('.accepted()', () => {
+        it('should get a list of accepted followers', (done) => {
+          sg.accepted(1, (users) => {
+            assert.equal(users.length, 0);
+            done();
+          });
+        });
+      });
+    });
+
+    it('should follow back user and add to `accepted`', (done) => {
+      sg.follow(11, 1).then((success) => {
+        assert(success);
+        client.smembers(`user:${1}:requested`, (err, res) => {
+          assert.equal(res[0], 11);
+          client.smembers(`user:${11}:pending`, (err2, res2) => {
+            assert.equal(res2[0], 1);
+            done();
+          });
+        });
+      });
+    });
+  });
 });
